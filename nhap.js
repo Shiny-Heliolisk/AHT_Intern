71dde70d20c2f5959cfffa63ae71122415bde3cc
pride <47897325+Shiny-Heliolisk@users.noreply.github.com>
2021-05-19 17:32:50 +0700

    Add files via upload

---
+++ nhap.js
@@ -0,0 +1,17 @@
+var isCalledOne = true;
+function myFunction(x) {
+  x.classList.toggle("change");
+  // document.getElementsByClassName("sidenav")[0].style.height = "500px";
+  
+  if(isCalledOne){
+    document.getElementsByClassName("sidenav")[0].style.height = "400px";
+    isCalledOne=false;
+  }
+  else{
+    document.getElementsByClassName("sidenav")[0].style.height = "0px";
+    isCalledOne=true;
+  }
+  
+}
+
+
